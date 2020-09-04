b3a124303ef0e1066845a2ba3291de04a4f63507
Magdiel JG <magdiel.juarez@grupojaque.com>
2020-09-03 21:10:25 -0500

    (FEAT): Try to learn git

--- models/user.js
+++ models/user.js
@@ -11,9 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Whe try to code
-      // Whe try to code
-      
+          // Whe try to code
+          // Whe try to code
+          // Whe try to code
+          // Whe try to code
+          // Whe try to code
+          // Whe try to code
     }
   };
   User.init({
