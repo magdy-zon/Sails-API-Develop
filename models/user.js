b037f60df7fa72f30acd49c3512091ab64f3a5d1
Magdiel JG <magdiel.juarez@grupojaque.com>
2020-09-03 21:10:25 -0500

    (FEAT): Try to learn git

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association her
     }
   };
   User.init({
@@ -20,5 +20,5 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'User',
   });
   return User;
-  
+
 };
