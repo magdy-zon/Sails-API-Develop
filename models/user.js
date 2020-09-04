d821b4e0a2ad891ff59564136efc1e4cb6347e07
Magdiel JG <magdiel.juarez@grupojaque.com>
2020-09-03 20:59:24 -0500

    (FEAT): Delete some commits

--- models/user.js
+++ models/user.js
@@ -11,11 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Whe try to code
-      // Whe try to code
-      // Whe try to code
-      // Whe try to code
-      // Whe try to code
     }
   };
   User.init({
